d045f6f36e36260c36f39ea0319216509c6cd52a
Jason Swartz <dev@bks2.com>
2026-06-29 10:36:14 -0700

    test: remove unused byok adapter import

--- tests/byok-cuecraft-adapter.test.ts
+++ tests/byok-cuecraft-adapter.test.ts
@@ -2,7 +2,6 @@ import { describe, expect, it } from "vitest";
 import {
 	applyCueCraftListedModels,
 	applyCueCraftModelRefreshFailure,
-	cueCraftByokSettingsFromCueCraftSettings,
 	cueCraftFetchedModelCount,
 	cueCraftModelRefreshMessage,
 	cueCraftProviderConfigFromSettings,
